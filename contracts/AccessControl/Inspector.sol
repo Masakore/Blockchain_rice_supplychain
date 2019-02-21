c7f3efcd95fe0f5891e8b6a101571b11e6b11b11
Masayuki <masakore8@gmail.com>
2019-02-21 19:44:36 +0800

    Add boilerplate to Consumer and Inspector

--- contracts/AccessControl/Inspector.sol
+++ contracts/AccessControl/Inspector.sol
@@ -3,7 +3,50 @@ pragma solidity ^0.4.0;
 import "./Roles.sol";
 
 contract Inspector {
-  function Inspector(){
+  using Roles for Roles.Role;
 
+  // Define 2 events, one for Adding, and other for Removing
+  event InspectorAdded(address indexed account);
+  event InspectorRemoved(address indexed account);
+
+  // Define a struct 'Inspectors' by inheriting from 'Roles' library, struct Role
+  Roles.Role private inspectors;
+
+  // In the constructor make the address that deploys this contract the 1st Inspector
+  constructor() public {
+    _addInspector(msg.sender);
+  }
+
+  // Define a modifier that checks to see if msg.sender has the appropriate role
+  modifier onlyInspector() {
+    require(isInspector(msg.sender))
+    _;
+  }
+
+  // Define a function 'isInspector' to check this role
+  function isInspector(address account) public view returns (bool) {
+    return inspector.has(account);
+  }
+
+  // Define a function 'addInspector' that adds this role
+  function addInspector(address account) public onlyInspector {
+    _addInspector(account);
+  }
+
+  // Define a function 'renounceInspector' to renounce this role
+  function renounceInspector() public {
+    _removeInspector(msg.sender);
+  }
+
+  // Define an internal function '_addInspector' to add this role, called by 'addInspector'
+  function _addInspector(address account) internal {
+    inspectors.add(account);
+    emit InspectorAdded(account);
+  }
+
+  // Define an internal function '_removeInspector' to remove this role, called by 'removeInspector'
+  function _removeInspector(address account) internal {
+    inspectors.remove(account);
+    emit InspectorRemoved(account);
   }
 }
