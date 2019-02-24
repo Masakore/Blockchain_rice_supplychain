6e0507ce27c7be03f038ae05ca4e2ee3199b0af4
Masayuki Osuka <masayuki@huubap.com>
2019-02-24 11:37:26 +0800

    Refactor indentation

--- contracts/AccessControl/Farmer.sol
+++ contracts/AccessControl/Farmer.sol
@@ -13,41 +13,41 @@ contract Farmer is AccessControl {
 	Roles.Role private farmers;
 
 
-// In the constructor make the address that deploys this contract the 1st farmer
-constructor() public {
-_addFarmer(msg.sender);
-}
+	// In the constructor make the address that deploys this contract the 1st farmer
+	constructor() public {
+		_addFarmer(msg.sender);
+	}
 
-// Define a modifier that checks to see if msg.sender has the appropriate role
-modifier onlyFarmer() {
-require(isFarmer(msg.sender));
-_;
-}
+	// Define a modifier that checks to see if msg.sender has the appropriate role
+	modifier onlyFarmer() {
+		require(isFarmer(msg.sender));
+		_;
+	}
 
-// Define a function 'isFarmer' to check this role
-function isFarmer(address account) public view returns (bool) {
-return farmers.has(account);
-}
+	// Define a function 'isFarmer' to check this role
+	function isFarmer(address account) public view returns (bool) {
+		return farmers.has(account);
+	}
 
-// Define a function 'addFarmer' that adds this role
-function addFarmer(address account) public onlyFarmer {
-_addFarmer(account);
-}
+	// Define a function 'addFarmer' that adds this role
+	function addFarmer(address account) public onlyFarmer {
+		_addFarmer(account);
+	}
 
-// Define a function 'renounceFarmer' to renounce this role
-function renounceFarmer() public {
-_removeFarmer(msg.sender);
-}
+	// Define a function 'renounceFarmer' to renounce this role
+	function renounceFarmer() public {
+		_removeFarmer(msg.sender);
+	}
 
-// Define an internal function '_addFarmer' to add this role, called by 'addFarmer'
-function _addFarmer(address account) internal {
-farmers.add(account);
-emit FarmerAdded(account);
-}
+	// Define an internal function '_addFarmer' to add this role, called by 'addFarmer'
+	function _addFarmer(address account) internal {
+		farmers.add(account);
+		emit FarmerAdded(account);
+	}
 
-// Define an internal function '_removeFarmer' to remove this role, called by 'removeFarmer'
-function _removeFarmer(address account) internal {
-farmers.remove(account);
-emit FarmerRemoved(account);
-}
+	// Define an internal function '_removeFarmer' to remove this role, called by 'removeFarmer'
+	function _removeFarmer(address account) internal {
+		farmers.remove(account);
+		emit FarmerRemoved(account);
+	}
 }
