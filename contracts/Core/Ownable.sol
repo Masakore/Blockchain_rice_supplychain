4ecd15a468ca278533289e5ace31c3eecfbc027b
Masayuki Osuka <masayuki@huubap.com>
2019-03-03 10:00:09 +0800

    Implement Ownable contract

--- contracts/Core/Ownable.sol
+++ contracts/Core/Ownable.sol
@@ -2,7 +2,40 @@ pragma solidity ^0.4.24;
 
 
 contract Ownable is SupplyChain {
-    function Core(){
+	address private origOwner;
 
-    }
+	event TransferOwnership(address indexed oldOwner, address indexed newOnwer);
+
+	constructor() internal {
+		origOwner = msg.sender;
+		emit TransferOwnership(address(0), origOwner);
+	}
+
+	function owner() public view returns (address) {
+		return origOwner;
+	}
+
+	modifier onlyOwner() {
+		require(isOwner());
+		_;
+	}
+
+	function isOwner() public view returns (bool) {
+		return msg.sender == origOwner;
+	}
+
+	function renounceOwnership() public onlyOwner {
+		emit TransferOwnership(origOwner, address(0));
+		origOwner = address(0);
+	}
+
+	function transferOwnership(address newOwner) public onlyOwner {
+		_transferOwnership(newOwner);
+	}
+
+	function _transferOwnership(address newOwner) internal {
+		require(newOwner != address(0));
+		emit TranferOwnership(origOwner, newOwner);
+		origOwner = newOwner;
+	}
 }
