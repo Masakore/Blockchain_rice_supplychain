35de314af53a467083f3a462e3371c92a1b73576
Masayuki Osuka <masayuki@huubap.com>
2019-03-03 10:38:11 +0800

    Add migration files

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,18 @@
+// migrating the appropriate contracts
+var FarmerRole = artifacts.require("../contracts/AccessControl/Farmer.sol");
+var InspectorRole = artifacts.require("../contracts/AccessControl/Inspector.sol");
+var DistributorRole = artifacts.require("../contracts/AccessControl/Distributor.sol");
+var RetailerRole = artifacts.require("../contracts/AccessControl/Retailer.sol");
+var ConsumerRole = artifacts.require("../contracts/AccessControl/Consumer.sol");
+var Ownable = artifacts.require("../contracts/Core/Ownable.sol");
+var SupplyChain = artifacts.require("../contracts/Core/SupplyChain.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(FarmerRole);
+  deployer.deploy(InspectorRole);
+  deployer.deploy(DistributorRole);
+  deployer.deploy(RetailerRole);
+  deployer.deploy(ConsumerRole);
+  deployer.deploy(Ownable);
+  deployer.deploy(SupplyChain);
+};
