b0575e96765611a178fb230d81edcccca012f32d
Masayuki Osuka <masayuki@huubap.com>
2019-03-11 08:12:26 +0800

    comment out Roles.sol because it causes migration error although not sue why

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,8 @@ var InspectorRole = artifacts.require("../contracts/AccessControl/Inspector.sol"
 var DistributorRole = artifacts.require("../contracts/AccessControl/Distributor.sol");
 var RetailerRole = artifacts.require("../contracts/AccessControl/Retailer.sol");
 var ConsumerRole = artifacts.require("../contracts/AccessControl/Consumer.sol");
-var Ownable = artifacts.require("../contracts/Core/Ownable.sol");
+// TODO After commeting out Ownable contract, successfully start test. But don't know why
+// var Ownable = artifacts.require("../contracts/Core/Ownable.sol");
 var SupplyChain = artifacts.require("../contracts/Core/SupplyChain.sol");
 
 module.exports = function(deployer) {
@@ -13,6 +14,6 @@ module.exports = function(deployer) {
   deployer.deploy(DistributorRole);
   deployer.deploy(RetailerRole);
   deployer.deploy(ConsumerRole);
-  deployer.deploy(Ownable);
+  // deployer.deploy(Ownable);
   deployer.deploy(SupplyChain);
 };
