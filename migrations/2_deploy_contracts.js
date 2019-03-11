5502537cc2f855a42ea8d5b4017faad9590fef1c
Masayuki Osuka <masayuki@huubap.com>
2019-03-11 08:16:21 +0800

    comment out Roles.sol because it is considered Embedded library so no need to separately compile

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,12 @@ var InspectorRole = artifacts.require("../contracts/AccessControl/Inspector.sol"
 var DistributorRole = artifacts.require("../contracts/AccessControl/Distributor.sol");
 var RetailerRole = artifacts.require("../contracts/AccessControl/Retailer.sol");
 var ConsumerRole = artifacts.require("../contracts/AccessControl/Consumer.sol");
-// TODO After commeting out Ownable contract, successfully start test. But don't know why
-// var Ownable = artifacts.require("../contracts/Core/Ownable.sol");
+/* Embedded Library: If a smart contract is consuming a library which have only internal functions than EVM simply embeds library into the contract. 
+Instead of using delegate call to call a function, it simply uses JUMP statement(normal method call). 
+There is no need to separately deploy library in this scenario.
+ */
+// this is library: var Ownable = artifacts.require("../contracts/Core/Ownable.sol");
+
 var SupplyChain = artifacts.require("../contracts/Core/SupplyChain.sol");
 
 module.exports = function(deployer) {
